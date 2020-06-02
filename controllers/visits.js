924c6281c18ad81b2164aa0c77d81a63e5da9a89
Kathleen Stickel <kstick@Kathleens-MBP.lan>
2020-06-01 21:41:55 -0500

    update new visit form

--- controllers/visits.js
+++ controllers/visits.js
@@ -1,8 +1,10 @@
-const Visit = require ('../models/visit');
+const User = require('../models/user');
+const Visit = require('../models/visit');
 
 module.exports = {
     index,
-    new: newVisit
+    new: newVisit,
+    create
 };
 
 function index(req, res) {
@@ -11,4 +13,15 @@ function index(req, res) {
 
 function newVisit(req, res) {
     res.render('new', {title: 'Add New Visit', user: req.user, parkName: Visit.schema.path('parkName').enumValues});
+}
+
+function create(req, res) {
+    const d = req.body.date;
+    req.body.date = `${d.substr(5, 2)}-${d.substr(8, 2)}-${d.substr(0, 4)}`;
+    // const visit = new Visit(req.body);
+    //! fix this
+    Visit.create(req.body, function(err, visit) {
+        console.log(req.body);
+        res.redirect('/visits');
+    })
 }
--- routes/visits.js
+++ routes/visits.js
@@ -3,6 +3,7 @@ const visitsCtrl = require('../controllers/visits');
 
 router.get('/', isLoggedIn, visitsCtrl.index);
 router.get('/new', isLoggedIn, visitsCtrl.new);
+router.post('/', isLoggedIn, visitsCtrl.create);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
