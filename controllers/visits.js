075d6e528692268fa447a48e7580214d8a6f45f4
Kathleen Stickel <kstick@Kathleens-MBP.lan>
2020-06-02 14:58:38 -0500

    update visit controllers and view

--- controllers/visits.js
+++ controllers/visits.js
@@ -8,20 +8,38 @@ module.exports = {
 };
 
 function index(req, res) {
-    res.render('visits', {title: 'Your visits', user: req.user });
+    // Visit.find({}, function(err, visits) {
+    //     console.log('req.user: ', req.user) //!console log
+    // });
+    User.findById(req.user).populate('parkVisits').exec(function(err, user) {
+        res.render('visits', {title: `${user.name}'s Visits`, user});
+    })
 }
 
 function newVisit(req, res) {
-    res.render('new', {title: 'Add New Visit', user: req.user, parkName: Visit.schema.path('parkName').enumValues});
+    res.render('new', {
+        title: 'Add New Visit', 
+        user: req.user, 
+        parkName: Visit.schema.path('parkName').enumValues});
 }
 
 function create(req, res) {
     const d = req.body.date;
     req.body.date = `${d.substr(5, 2)}-${d.substr(8, 2)}-${d.substr(0, 4)}`;
-    // const visit = new Visit(req.body);
-    //! fix this
-    Visit.create(req.body, function(err, visit) {
-        console.log(req.body);
+    user = req.user;
+    const visit = new Visit(req.body);
+    visit.save(function(err) {
+        if (err) {
+            console.log('error: ', err); //! console log
+            return res.redirect('/visits/new');
+        }
+        user.parkVisits.push(visit);
+        user.save(function(err) {
+            if (err) {
+                console.log('error: ', err); //! console log
+                return res.redirect('/visits/new');
+            }
+        })
         res.redirect('/visits');
-    })
+    })    
 }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Visit = require('../models/visit');
 
 const userSchema = new Schema({
     name: String,
